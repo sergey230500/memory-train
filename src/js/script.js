548ae5471f268121fd3a82cc61fe1c55967547b1
blr-Oliver <vasily.liaskovsky@gmail.com>
2019-08-19 23:44:54 +0300

    черновик скрипта

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+function MemoryGame(){
+  // какие-то поля
+  this.previousValue = null;
+}
+
+MemoryGame.prototype = {
+  init: function(){},
+  startGame: function(){},
+  startChain: function(){},
+  continueChain: function(value){},
+  updateTimer: function(){},
+  timerOverdue: function(){},
+  stopGame: function(){}
+
+  // обработчики событий
+};
