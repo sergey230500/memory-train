c317a26273245ce2ac9bcc84a4b87eebb8dab043
blr-Oliver <vasily.liaskovsky@gmail.com>
2019-08-21 00:03:27 +0300

    привязка к дереву страницы

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,25 @@
 function MemoryGame(){
-  // какие-то поля
   this.previousValue = null;
+  this.displayText = null;
+  this.progressBar = null;
+  this.progressText = null;
+  this.userInput = null;
+  this.gameForm = null;
 }
 
 MemoryGame.prototype = {
-  init: function(){},
+  init: function(){
+    this.displayText = document.querySelector ('.display-text');
+    this.progressBar = document.querySelector('.progress .progress-bar');
+    this.progressText = document.querySelector('.progress .progress-text');
+    this.userInput = document.querySelector('#answer');
+    this.gameForm = document.querySelector('form');
+
+    this.attachListeners();
+  },
+  attachListeners: function(){
+
+  },
   startGame: function(){},
   startChain: function(){},
   continueChain: function(value){},
@@ -14,3 +29,6 @@ MemoryGame.prototype = {
 
   // обработчики событий
 };
+
+var game = new MemoryGame();
+game.init();
